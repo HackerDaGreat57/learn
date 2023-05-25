4ff029f5140daef9a7ae58eb840d405e0f096170
HackerDaGreat57 <HackerDaGreat57@gmail.com>
2023-05-24 18:08:20 -0700

    create template file.

---
+++ template.cpp
@@ -0,0 +1,10 @@
+//template.cpp: this file is used to calibrate the flibbertigibbet
+
+//Includes
+#include <iostream> //Console streams (essential)
+
+int main(int argc, char *argv[]) {
+  //figure out how to hack into the fbi's nude collection here
+
+  return 0;
+}
