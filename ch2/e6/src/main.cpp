3e11c4ab9037ba8f6eb3850c1a10a23d03a1fd0f
HackerDaGreat57 <HackerDaGreat57@gmail.com>
2023-05-26 16:56:53 -0700

    I spent way too much time on ch2_e6.

--- ch2/e6/src/main.cpp
+++ ch2/e6/src/main.cpp
@@ -1,12 +1,23 @@
-//main.cpp: produce a table with some numeric values and Unicode characters
+//main.cpp: produce a table with some common numeric values and Unicode characters
 
 //Includes
 #include <iostream> //Console streams (essential)
 #include <format> //Output text manipulation
 
 int main(int argc, char *argv[]) {
-  std::cout << std::format("{:*<52}", "") << std::endl;
-  std::cout << std::format("| {:15}| {:15}| {:15}|", "Constant", "Value", "Approx. Value") << std::endl;
+  std::cout << "╔═════════════════════╤═════════════════════╤═════════════════════╗" << std::endl;
+  std::cout << std::format("║ {:20}│ {:20}│ {:20}║", "Constant", "Description", "Approx. Value") << std::endl;
+  std::cout << "╟━━━━━━━━━━━━━━━━━━━━━┿━━━━━━━━━━━━━━━━━━━━━┿━━━━━━━━━━━━━━━━━━━━━╢" << std::endl;
+  std::cout << std::format("║ {:20}│ {:20}│ {:<20}║", "std::numbers::e", "The base of the", std::numbers::e) << std::endl;
+  std::cout << std::format("║ {:20}│ {:20}│ {:20}║", " ", "natural logarithm", " ") << std::endl;
+  std::cout << "╟─────────────────────┼─────────────────────┼─────────────────────╢" << std::endl;
+  std::cout << std::format("║ {:20}│ {:20} │ {:<20}║", "std::numbers::pi", "π", std::numbers::pi) << std::endl;
+  std::cout << "╟─────────────────────┼─────────────────────┼─────────────────────╢" << std::endl;
+  std::cout << std::format("║ {:20}│ {:20}│ {:<20}║", "std::numbers::sqrt2", "Square root of 2", std::numbers::sqrt2) << std::endl;
+  std::cout << "╟─────────────────────┼─────────────────────┼─────────────────────╢" << std::endl;
+  std::cout << std::format("║ {:20}│ {:20}│ {:<20}║", "std::numbers::phi", "The golden ratio", std::numbers::e) << std::endl;
+  std::cout << std::format("║ {:20}│ {:20} │ {:20}║", " ", "constant φ", " ") << std::endl;
+  std::cout << "╚═════════════════════╧═════════════════════╧═════════════════════╝" << std::endl;
 
   return 0;
 }
