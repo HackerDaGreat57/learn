48e42be119415c894a373eea37178b0ce238bdc1
HackerDaGreat57 <HackerDaGreat57@gmail.com>
2023-05-26 12:25:59 -0700

    ch2_e4 was using mul instead of div :facepalm: (it works now)

--- ch2/e4/src/main.cpp
+++ ch2/e4/src/main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]) {
   std::cout << "Please enter your height. Enter the number of feet, then the number of inches, separated by a space: ";
   std::cin >> height_feet >> height_inches;
 
-  long double weight_c = weight * 2.2; //1 kilogram = 2.2 pounds
+  long double weight_c = weight / 2.2; //1 kilogram = 2.2 pounds
   long double height_c = (height_feet * 0.3048) + ((height_inches / 12) * 0.3048); //1 foot is 0.3048 meters
   long double bmi = weight_c / (height_c * height_c);
 
