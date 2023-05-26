f502754d5c2219017c070b9dae8accc39ffb75b3
HackerDaGreat57 <HackerDaGreat57@gmail.com>
2023-05-26 10:02:40 -0700

    Why isn't this working??

---
+++ ch2/e4/src/main.cpp
@@ -0,0 +1,24 @@
+//main.cpp: calculate my BMI from
+
+//Includes
+#include <iostream> //Console streams (essential)
+#include <cmath> //Mathematical functions
+
+int main(int argc, char *argv[]) {
+  long double weight;
+  std::cout << "Please enter your weight in pounds: ";
+  std::cin >> weight;
+
+  long double height_feet;
+  long double height_inches;
+  std::cout << "Please enter your height. Enter the number of feet, then the number of inches, separated by a space: ";
+  std::cin >> height_feet >> height_inches;
+
+  long double weight_c = weight * 2.2; //1 kilogram = 2.2 pounds
+  long double height_c = (height_feet * 0.3048) + ((height_inches / 12) * 0.3048); //1 foot is 0.3048 meters
+  long double bmi = weight_c / (height_c * height_c);
+
+  std::cout << std::endl << "Your BMI is " << bmi << std::endl;
+
+  return 0;
+}
