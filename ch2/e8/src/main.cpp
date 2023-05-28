1cd8cf523e96e2821e0ebfeb320be28249ff2a12
HackerDaGreat57 <HackerDaGreat57@gmail.com>
2023-05-27 17:58:10 -0700

    Wish me luck figuring this one out

---
+++ ch2/e8/src/main.cpp
@@ -0,0 +1,16 @@
+//main.cpp: identifies the larger of 2 positive integers
+
+//Includes
+#include <iostream> //Console streams (essential)
+#include <stdint.h> //Integers (uint64_t)
+
+int main(int argc, char *argv[]) {
+  uint64_t int1;
+  uint64_t int2;
+  std::cout << "Please enter the first and second integers, separated by a space: ";
+  std::cin >> int1 >> int2;
+
+  //WHAT
+
+  return 0;
+}
